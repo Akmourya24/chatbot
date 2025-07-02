50928350fd7d69cd7e3c9c3da711c367d06b7c19
Alok Mourya <alokmourya44@gmail.com>
2025-07-02 11:44:47 +0530

    Implement user login functionality with token generation and cookie handling; add options for secure cookies

--- backend/controller/user.controller.js
+++ backend/controller/user.controller.js
@@ -2,6 +2,28 @@ import { User } from "../models/user.model.js"
 import ApiError from "../utils/apiError.js"
 import asyncHandler from "../utils/asyncHandler.js"
 import { ApiResponse } from "../utils/ApiResponse.js"
+import { option } from "../utils/option.js"
+
+// create accesstoken and refreshtoken
+const generateAccessTokenAndRefreshToken = asyncHandler(async (userId) => {
+    try {
+        const user = await User.findById(userId)
+        const accessToken = User.generateAccessToken()
+        const refreshToken = User.generateRefreshToken()
+
+        user.refreshToken = refreshToken
+        await user.save({ validateBeforeSave: false })
+
+        return (
+            accessToken,
+            refreshToken
+        )
+
+    } catch (error) {
+        throw new ApiError(500, "Error getting tokens ")
+
+    }
+})
 
 const registerUser = asyncHandler(async (req, res) => {
     try {
@@ -39,14 +61,52 @@ const registerUser = asyncHandler(async (req, res) => {
         return res.status(200).
             json(new ApiResponse(200, createdUser, "User registered successfully"))
 
-        } catch (error) {
-            throw new ApiError(
-                error.statusCode || 500,
-                error.message || "Internal Server Error"
-            )
+    } catch (error) {
+        throw new ApiError(
+            error.statusCode || 500,
+            error.message || "Internal Server Error"
+        )
 
     }
 
 })
 
-export { registerUser }
+const loginUser = asyncHandler(async (req, res) => {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+        throw new ApiError(404, "email or password is requird")
+
+    }
+    const userfind = await User.findOne({
+        $or: [{ email }]
+
+    })
+    if (!userfind) {
+        throw new ApiError(404, "User does  not exist")
+    }
+    const verifyPassword = await userfind.isPasswordCorrect(password)
+    if (!verifyPassword) {
+        throw new ApiError(401, "password is incorrect")
+    }
+
+    const { accessToken, refreshToken } = await generateAccessTokenAndRefreshToken(userfind._id)
+
+    const userLoggedin = await User.findById(userfind._id).select(
+        "-password -refreshToken"
+    )
+    console.log("User login succesfully", userLoggedin)
+
+    return res
+        .status(200).
+        cookie("refreshToken", refreshToken, option).
+        cookie("accessToken", accessToken, option).
+        json(
+            new ApiResponse(200,
+                { user: userLoggedin, accessToken, refreshToken },
+                "user logged in successfully"
+            )
+        )
+})
+
+export { registerUser ,loginUser }
